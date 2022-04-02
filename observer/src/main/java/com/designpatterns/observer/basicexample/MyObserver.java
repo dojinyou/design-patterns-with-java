8919ec89e50a545168b8948321c78f3c1e125468
dojinyou <djyou128@gmail.com>
2022-04-02 12:34:16 +0900

    chore: git ignore 추가

--- observer/src/main/java/com/designpatterns/observer/basicexample/MyObserver.java
+++
@@ -1,5 +0,0 @@
-package com.designpatterns.observer.basicexample;
-
-public interface MyObserver {
-    void update ();
-}
--- observer/src/main/java/com/designpatterns/observer/basicexample/Store.java
+++
@@ -1,8 +0,0 @@
-package com.designpatterns.observer.basicexample;
-
-public class Store implements Subject {
-    public void arriveNewItem() {
-        System.out.println("Arrive New Item");
-        this.notifyObservers();
-    }
-}
